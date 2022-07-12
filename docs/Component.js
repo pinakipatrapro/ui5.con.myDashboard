ee1a031082ab9ac8f694c949d963ace3eff3a803
Pinaki patra <pinaki.patra.pro@gmail.com>
2022-07-12 15:01:29 +0000

    Build Updated

---
+++ docs/Component.js
@@ -0,0 +1,1 @@
+sap.ui.define(["sap/ui/core/UIComponent","sap/ui/Device","ui5/con/myDashboard/model/models"],function(e,i,t){"use strict";return e.extend("ui5.con.myDashboard.Component",{metadata:{manifest:"json"},init:function(){e.prototype.init.apply(this,arguments);this.getRouter().initialize();this.setModel(t.createDeviceModel(),"device")}})});
---
+++ docs/test/integration/arrangements/Startup.js
@@ -0,0 +1,1 @@
+sap.ui.define(["sap/ui/test/Opa5"],function(t){"use strict";return t.extend("ui5.con.myDashboard.test.integration.arrangements.Startup",{iStartMyApp:function(){this.iStartMyUIComponent({componentConfig:{name:"ui5.con.myDashboard",async:true,manifest:true}})}})});
