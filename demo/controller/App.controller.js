81a953b04e828af395e2daa1cd9ffb382e3e8d22
Pinaki Patra <32954893+pinakipatrapro@users.noreply.github.com>
2022-07-12 20:25:51 +0530

    Delete demo directory

--- demo/controller/App.controller.js
+++
@@ -1,1 +0,0 @@
-sap.ui.define(["sap/ui/core/mvc/Controller"],function(n){"use strict";return n.extend("ui5.con.myDashboard.controller.controller.App",{onInit(){}})});
--- demo/test/integration/arrangements/Startup.js
+++
@@ -1,1 +0,0 @@
-sap.ui.define(["sap/ui/test/Opa5"],function(t){"use strict";return t.extend("ui5.con.myDashboard.test.integration.arrangements.Startup",{iStartMyApp:function(){this.iStartMyUIComponent({componentConfig:{name:"ui5.con.myDashboard",async:true,manifest:true}})}})});
